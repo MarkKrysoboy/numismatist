fff2dd6c00850dab3c9ea4d67d2bcdcf3ef78ac5
Mark Krysoboy <heter1209@gmail.ru>
2022-12-21 11:30:49 +0300

    Initial commit

---
+++ src/main/java/com/example/numismatist/NumismatistApplication.java
@@ -0,0 +1,13 @@
+package com.example.numismatist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumismatistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumismatistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/numismatist/NumismatistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numismatist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumismatistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
